95d20fed17b6bc4f5d7c0461617e7b2ff7591986
Ashish Jullia <ashishjullia19@gmail.com>
2020-11-05 15:27:28 -0500

    ONE-85 Generate SSH keys

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -11,5 +11,22 @@ const jsonwebtoken = require('jsonwebtoken');
 
 // Create SSH keys
 exports.generatekeys = async (req, res, next) => {
+    
+    var location = __dirname + '/../keys/foo_rsa';
+    var comment = 'comment';
+    var password = '';
+    var format = 'PEM';
 
+    keygen({
+        location: location,
+        read: true,
+        comment: comment,
+        password: password,
+        format: format
+    },  function(err, out){
+        if(err) return console.log('Something went wront: '+err);
+        console.log('keys created!');
+        console.log('private key: '+out.key);
+        res.json({ message: "keys created!" });
+    });
 };
